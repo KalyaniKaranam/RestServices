682a395d3392dc998e521b14b361cf40f05821ae
KalyaniKaranam <30898080+KalyaniKaranam@users.noreply.github.com>
2017-09-25 17:28:57 +0530

    Add files via upload

---
+++ RestService/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RestService
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            GetJson();
+        }
+        public async void GetJson()
+        {
+            if (NetworkCheck.IsInternet())
+            {
+                var client = new HttpClient();
+                var response = await client.GetAsync("http://www.groupkt.com/post/c9b0ccb9/country-and-other-related-rest-webservices.html");
+                string sampleJson = await response.Content.ReadAsStringAsync();
+                CountryList con = new CountryList();
+                if (sampleJson != "")
+                {
+                    con = JsonConvert.DeserializeObject<CountryList>(con.Countries.ToString());
+                    listviewCountry.ItemsSource = con.Countries;
+                }
+                else
+                {
+                    await DisplayAlert("Json Parsing", "No Network is Available", "OK");
+                }
+                ProgressLoader.IsVisible = false;
+            }
+
+        }
+
+        private void listviewCountry_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ RestService/model.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestService
+{
+    public class Model
+    {
+        //public async void GetJson()
+        //{
+        //    if(NetworkCheck.IsInternet())
+        //    {
+        //        var client = new HttpClient();
+        //        var response = await client.GetAsync("http://www.groupkt.com/post/c9b0ccb9/country-and-other-related-rest-webservices.html");
+        //        string sampleJson = await response.Content.ReadAsStringAsync();
+        //        CountryList con = new CountryList();
+        //        if(sampleJson!="")
+        //        {
+        //            con = JsonConvert.DeserializeObject<CountryList>(con.Countries.ToString());
+        //            listviewCountry.ItemSource = con.Countries;
+        //        }
+        //        else
+        //        {
+        //            await DisplayAlert("Json Parsing", "No Network is Available", "OK");
+        //        }
+        //        ProgressLoader.IsVisible = false;
+        //    }
+           
+       // }
+    }
+}
